86b95b1013038a93cf7c0afe3b5ea4a7dd9bdfec
Tushar Tari <tushartari11@gmail.com>
2025-03-02 01:36:23 +0100

    addding docker support

---
+++ notification-service/src/main/java/com/rekreation/store/notification/service/NotificationService.java
@@ -0,0 +1,55 @@
+package com.rekreation.store.notification.service;
+
+
+import com.rekreation.store.order.event.OrderPlacedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationService {
+
+    private final JavaMailSender javaMailSender;
+
+    @KafkaListener(topics = "order_placed")
+    public void listenOrderPlaced(OrderPlacedEvent orderPlacedEvent) {
+        log.info("Notification received from order-placed topic: {}", orderPlacedEvent);
+
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("springshop@email.com");
+            messageHelper.setTo(orderPlacedEvent.getEmail().toString());
+            messageHelper.setSubject(String.format("Your Order with OrderNumber %s is placed successfully", orderPlacedEvent.getOrderNumber()));
+            messageHelper.setText(String.format("""
+                            Hi %s %s,
+
+                            Your order with order number %s is now placed successfully.
+
+                            Best Regards
+                            Spring Shop
+                            """,
+                    trimToEmptyIfNull(orderPlacedEvent.getFirstName()),
+                    trimToEmptyIfNull(orderPlacedEvent.getLastName()),
+                    orderPlacedEvent.getOrderNumber()));
+        };
+        try {
+            javaMailSender.send(messagePreparator);
+            log.info("Order Notification email sent!!");
+        } catch (MailException e) {
+            log.error("Exception occurred when sending mail", e);
+            throw new RuntimeException("Exception occurred when sending mail to springshop@email.com", e);
+        }
+    }
+
+    private String trimToEmptyIfNull(CharSequence stringInput) {
+        return stringInput!=null ? stringInput.toString() : "";
+    }
+}
