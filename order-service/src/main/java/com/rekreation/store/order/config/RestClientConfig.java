f6f50c5e48b691333825513a83b8691e40ca3475
Tushar Tari <tushartari11@gmail.com>
2025-03-03 16:13:35 +0100

    addding kubernetes final changes

--- order-service/src/main/java/com/rekreation/store/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/rekreation/store/order/config/RestClientConfig.java
@@ -4,8 +4,8 @@ import com.rekreation.store.order.client.InventoryClient;
 import io.micrometer.observation.ObservationRegistry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
-import org.springframework.boot.http.client.ClientHttpRequestFactorySettings;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
@@ -13,7 +13,7 @@ import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
-import static java.time.Duration.ofSeconds;
+import java.time.Duration;
 
 @Configuration
 @RequiredArgsConstructor
@@ -37,9 +37,9 @@ public class RestClientConfig {
     }
 
     private ClientHttpRequestFactory getClientRequestFactory() {
-        ClientHttpRequestFactorySettings clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.defaults()
-                .withConnectTimeout(ofSeconds(3))
-                .withReadTimeout(ofSeconds(3));
-        return ClientHttpRequestFactoryBuilder.detect().build(clientHttpRequestFactorySettings);
+        ClientHttpRequestFactorySettings clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(3))
+                .withReadTimeout(Duration.ofSeconds(3));
+        return ClientHttpRequestFactories.get(clientHttpRequestFactorySettings);
     }
 }
